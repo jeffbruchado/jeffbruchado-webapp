c82a07837923855df93f495987327eedd0d45b7f
Jeff Bruchado <jefferson.bruchado@neoway.com.br>
2022-04-19 12:33:42 -0300

    fix tablet logo

--- src/components/shared/logos/JeffBruchado.vue
+++ src/components/shared/logos/JeffBruchado.vue
@@ -10,12 +10,12 @@ defineProps({
    <router-link class="mr-3 flex-none overflow-hidden md:w-auto" to="/">
       <img
          v-if="colorMode === 'dark'"
-         class="lg:w-56 md:w-48 sm:w-32 w-48 h-auto"
+         class="lg:w-56 md:w-48 w-48 h-auto"
          src="@/assets/images/logo-vector-negative.svg"
       />
       <img
          v-else
-         class="lg:w-56 md:w-48 sm:w-32 w-48 h-auto"
+         class="lg:w-56 md:w-48 w-48 h-auto"
          src="@/assets/images/logo-vector.svg"
       />
    </router-link>
